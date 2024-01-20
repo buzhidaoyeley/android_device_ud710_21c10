1b184c430db307e47adbfd9e37a3a7576fd421f3
Captain Throwback <captainthrowback@hotmail.com>
2024-01-20 15:14:53 -0500

    I003D: move prebuilt modules to folder
    Add flag for custom versioning to build
    
    Change-Id: Iee818cf137d65ca2754e7988635bc592b8c9670b

--- device.mk
+++ device.mk
@@ -24,7 +24,7 @@
 $(call inherit-product, $(COMMON_PATH)/common.mk)
 
 # Copy modules for depmod
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*.ko,$(DEVICE_PATH)/prebuilt,$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*.ko,$(DEVICE_PATH)/prebuilt/modules,$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1)
 
 # Custom ROM asserts
 TARGET_OTA_ASSERT_DEVICE := I003D,ZS661KS
