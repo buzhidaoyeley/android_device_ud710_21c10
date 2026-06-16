5723ec7a30427746b7f05f82a729ab0b00c42e04
buzhidao <105332050+buzhidaoyeley@users.noreply.github.com>
2026-06-16 18:07:53 +0800

    Update product name and inherit common configuration

--- omni_ud710_21c10.mk
+++ omni_ud710_21c10.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from ud710_21c10 device
 $(call inherit-product, device/sprd/ud710_21c10/device.mk)
 
 PRODUCT_DEVICE := ud710_21c10
-PRODUCT_NAME := omni_ud710_21c10
+PRODUCT_NAME := twrp_ud710_21c10
 PRODUCT_BRAND := SPRD
 PRODUCT_MODEL := ud710_21c10_native
 PRODUCT_MANUFACTURER := sprd
